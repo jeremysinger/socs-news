06faae6edb0599366e9c4c23d5bae51631630732
Jeremy Singer <jeremy.singer@glasgow.ac.uk>
2015-03-27 17:41:21 +0000

    Now reads recent tweets from @glasgowcs

--- news.rb
+++ news.rb
@@ -6,6 +6,8 @@
 
 
 require 'mechanize'
+require 'rubygems'
+require 'twitter'
 
 mechanize = Mechanize.new
 
@@ -16,3 +18,12 @@ page.search('h2').each do |headline|
   puts headline.text.strip
 end
 
+
+client = Twitter::REST::Client.new do |config|
+  config.consumer_key    = "mGUOo7My0nIppSb5QkvTwnpFz"
+  config.consumer_secret = File.open('twitter.secret') {|f| f.readline}
+end
+
+client.search("glasgowcs", result_type: "recent").take(20).each do |tweet|
+  puts tweet.text
+end
